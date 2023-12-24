89ce195c23c6c940db5950f6e622a3b93c3d433d
Bodhisattwa <jiko.bhatt@gmail.com>
2023-12-24 16:02:17 +0530

    Added one more file

--- cicd/src/main/java/Main/Main.java
+++ cicd/src/main/java/Main/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 	System.out.println("Hi!, hello there");
 	System.out.println("Master");
+	System.out.println("Added one more file");
 	}
 }
